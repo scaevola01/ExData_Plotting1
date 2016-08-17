a6386eee1979c3cafed1a06c4e170dbb9b13ee9c
scaevola01 <jlynch@humnet.ucla.edu>
2016-08-17 06:50:15 -0700

    Adding comments

--- plot3.R
+++ plot3.R
@@ -1,8 +1,15 @@
 plot3 <- function(x) {
+  #Read in the data
   power <- read.csv("poweruse.txt", sep = ";", stringsAsFactors = FALSE)
+  
+  #Subset the dates
   power$Date <- strptime(power$Date, "%d/%m/%Y")
   power <- subset(power, Date == "2007-02-01"|Date == "2007-02-02")
+  
+  #Get the right time associated with the dates, so that it plots correctly
   power$newDate <- with(power, paste(Date, Time))
+  
+  #Plot the graph
   png(filename = "plot3.png")
   plot(as.POSIXct(power$newDate), power$Sub_metering_1, type='n', xlab = "", ylab = "Energy sub metering")
   lines(as.POSIXct(power$newDate), power$Sub_metering_1)
